b833ed91e09555473657929a155f46f919a25d1b
Juan Caicedo <juan@spanishdict.com>
2016-10-06 15:45:09 -0500

    solucion

---
+++ solucion/index.js
@@ -0,0 +1,48 @@
+#! /usr/bin/env node
+'use strict';
+const commander = require('commander');
+const inquirer = require('inquirer');
+
+function leerArgumentos(args) {
+   return commander
+    .option('-r, --repo <user/repo>', 'El nombre de un repo de GitHub')
+    .parse(args);
+}
+
+function preguntarRepoSiFalta(programa) {
+  let repo = programa.repo;
+
+  if(!programa.repo) {
+    return inquirer.prompt([{
+        name: 'repo',
+        message: 'Que repo te interesa?'
+      }])
+      .then(function(respuesta) {
+        return respuesta.repo;
+      });
+  }
+
+  return repo;
+}
+
+function imprimir(repo) {
+  console.log(repo);
+}
+
+function cogerErrores(error) {
+  console.error('Hubo un error inesperado!');
+  console.error('Por favor mándale un mensaje a los desarrolladores con:');
+  console.error(error.stack);
+}
+
+function main() {
+  Promise.resolve(process.argv)
+    .then(leerArgumentos)
+    .then(preguntarRepoSiFalta)
+    .then(imprimir)
+    .catch(cogerErrores);
+}
+
+if (require.main === module) {
+  main();
+}
